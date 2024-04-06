8465efe3d85d2a21bbec82b6e7cb5783ec9492ae
jignesh <85551495+jigneshbhai@users.noreply.github.com>
2024-04-06 12:09:17 +0530

    Header Footer Auth with clerk done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
 
 import './globals.css'
 
@@ -24,9 +25,11 @@ export default function RootLayout({
 }) {
   return (
    
+      <ClerkProvider>
       <html lang="en">
         <body className={poppins.variable}>{children}</body>
       </html>
+    </ClerkProvider>
    
   )
 }
--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href='/'>
+          <Image 
+            src="/assets/images/logo.png"
+            alt="logo"
+            width={200}
+            height={38}
+          />
+        </Link>
+
+        <p>2024 Influenzeer. All Rights reserved.</p>
+      </div>
+    </footer>
   )
 }
 
